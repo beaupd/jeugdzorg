0e5d65f05dc6c4ff3765d79572e9f18e5c2f74e5
ju5tu5 <justus@ju5tu5.nl>
2022-04-04 10:16:44 +0200

    Added competentie

---
+++ models/competentie.model.js
@@ -0,0 +1,134 @@
+const db = require('./db')
+const helper = require('./helper')
+
+/**
+ * Constructor for new competenties that checks if the passed object adheres the
+ * format we need and throws errors if it doesn't
+ * @param {*} competentie an object containing the necessary fields to make a new competentie
+ */
+const competentie = function (competentie) {
+  // TODO: Check for sanity...
+  this.competentieId = competentie.competentieId
+  this.naam = competentie.naam
+}
+
+/**
+ * Get all competenties from the database, will be paginated if the number of
+ * competenties in the database exceeds process.env.LIST_PER_PAGE
+ * @param {*} page the page of authors you want to get
+ * @returns
+ */
+competentie.get = async function (page = 1) {
+  const rows = await db.query(`SELECT * FROM competentie LIMIT ?,?`, [
+    helper.getOffset(page, process.env.LIST_PER_PAGE),
+    Number(process.env.LIST_PER_PAGE),
+  ])
+
+  return {
+    data: helper.emptyOrRows(rows),
+    meta: { page },
+  }
+}
+
+/**
+ *
+ * @param {*} competentieId
+ * @returns
+ */
+competentie.getById = async function (competentieId) {
+  const rows = await db.query(`SELECT * FROM competentie WHERE competentieId = ?`, [competentieId])
+  return {
+    data: helper.emptyOrRows(rows),
+    meta: { page },
+  }
+}
+
+/**
+ * Add a new competentie to the database
+ * @param {*} competentie a new competentie object created with the competentie constructor
+ * @returns an object containing the inserted competentie with the newly inserted competentieId
+ */
+competentie.post = async function (competentie) {
+  const rows = await db.query(
+    `INSERT INTO competentie SET ${prepareQuery(competentie)}`,
+    prepareParams(competentie)
+  )
+  competentie.competentieId = rows.insertId
+  return {
+    data: [competentie],
+    meta: {
+      insertId: rows.insertId,
+    },
+  }
+}
+
+/**
+ * Patch a competentie in the database
+ * @param {*} competentie a competentie object to be patched containing at least
+ *                        the competentieId and the fields to patch
+ * @returns
+ */
+competentie.patch = async function (competentie) {
+  const rows = await db.query(
+    `UPDATE competentie SET ${prepareQuery(competentie)} WHERE competentieId = ?`,
+    prepareParams(competentie)
+  )
+  return {
+    data: helper.emptyOrRows(rows),
+    meta: {},
+  }
+}
+
+/**
+ * Put a competentie in the database
+ * @param {*} competentie a competentie object to be put
+ * @returns
+ */
+competentie.put = async function (competentie) {
+  const rows = await db.query(
+    `UPDATE competentie SET ${prepareQuery(competentie)} WHERE competentieId = ?`,
+    prepareParams(competentie)
+  )
+  return {
+    data: helper.emptyOrRows(rows),
+    meta: {},
+  }
+}
+
+/**
+ * Delete a competentie from the database
+ * @param {*} competentieId the competentieId from the competentie to be deleted
+ * @returns
+ */
+competentie.delete = async function (competentieId) {
+  const rows = await db.query(`DELETE FROM competentie WHERE competentieId = ?`, [competentieId])
+  return {
+    data: helper.emptyOrRows(rows),
+    meta: {},
+  }
+}
+
+module.exports = competentie
+
+/**
+ * Prepares part of an SQL query based on a passed partial competentie object
+ * @param {*} competentie partial competentie object containing at least the competentieId
+ * @returns a string to be used in the patch query, eg 'field = ?, field2 = ? ...'
+ */
+function prepareQuery(competentie) {
+  return Object.keys(competentie)
+    .filter((field) => field != 'competentieId')
+    .map((field) => `${field} = ?`)
+    .reduce((prev, curr) => `${prev}, ${curr}`)
+}
+
+/**
+ * Prepares a passed partial competentie object for querying the database. Whatever
+ * fields are passed, the competentieId needs to be at the end.
+ * @param {*} competentie partial competentie object containing at least the competentieId
+ * @returns [] an array to be used in the patch query
+ */
+function prepareParams(competentie) {
+  const { competentieId, ...preparedcompetentie } = competentie
+  return [...Object.values(preparedcompetentie), competentieId]
+}
---
+++ routes/competentie.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const Competentie = require('../models/competentie.model')
+
+module.exports = express
+  .Router()
+
+  // Add a new item
+  .post('/', async (req, res, next) => {
+    try {
+      console.log('Got: ', req.body)
+      res.json(await Competentie.create(new Competentie(req.body)))
+    } catch (err) {
+      console.error('Error while adding: ', err.message)
+      next(err)
+    }
+  })
+
+  // List ALL items
+  .get('/', async (req, res, next) => {
+    try {
+      res.json(await Competentie.getAll(req.query.page))
+    } catch (err) {
+      console.error('Error while getting all: ', err.message)
+      next(err)
+    }
+  })
+
+  // Get a specific item
+  .get('/:id', async (req, res, next) => {
+    try {
+      res.json(await Competentie.getById(req.params.id))
+    } catch (err) {
+      res.json({
+        message: `Error while getting specific: ${err.message}`,
+        request: req.body,
+      })
+    }
+  })
+
+  // Put an item
+  .put('/', async (req, res, next) => {
+    try {
+      res.json(await Competentie.put(new Competentie(req.body)))
+    } catch (err) {
+      res.json({
+        message: `Error while putting: ${err.message}`,
+        request: req.body,
+      })
+    }
+  })
+
+  // Patch an item
+  .patch('/', async (req, res, next) => {
+    try {
+      res.json(await Competentie.patch(req.body))
+    } catch (err) {
+      res.json({
+        message: `Error while patching: ${err.message}`,
+        request: req.body,
+      })
+    }
+  })
+
+  // Delete an item
+  .delete(['/', '/:id'], async (req, res, next) => {
+    try {
+      res.json(await Competentie.delete(req.params.id || req.body.id))
+    } catch (err) {
+      res.json({
+        message: `Error while deleting: ${err.message}`,
+        request: `You tried to delete id: ${req.params.id || req.body.id}`,
+      })
+    }
+  })
