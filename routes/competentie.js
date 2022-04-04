f9f7787310da0d996a5ece1f23029f99d5249c7d
ju5tu5 <justus@ju5tu5.nl>
2022-04-04 10:31:33 +0200

    Update competentie.js

--- routes/competentie.js
+++ routes/competentie.js
@@ -18,7 +18,7 @@ module.exports = express
   // List ALL items
   .get('/', async (req, res, next) => {
     try {
-      res.json(await Competentie.getAll(req.query.page))
+      res.json(await Competentie.get(req.query.page))
     } catch (err) {
       console.error('Error while getting all: ', err.message)
       next(err)
