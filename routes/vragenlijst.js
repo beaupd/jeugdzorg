8b79e00ebfdd1dc0908b8ca2545ab33f0a67838c
Justus Sturkenboom <justus@ju5tu5.nl>
2022-04-07 11:17:20 +0200

    Update vragenlijst.js
    Renamed function create to post

--- routes/vragenlijst.js
+++ routes/vragenlijst.js
@@ -8,7 +8,7 @@ module.exports = express
   .post('/', async (req, res, next) => {
     try {
       console.log('Got: ', req.body)
-      res.json(await Vragenlijst.create(new Vragenlijst(req.body)))
+      res.json(await Vragenlijst.post(new Vragenlijst(req.body)))
     } catch (err) {
       console.error('Error while adding: ', err.message)
       next(err)
